0cfea14713c87a28253916bb7309b003225a0453
Cailie Skelton <cskelton707@gmail.com>
2020-05-01 16:09:19 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,20 @@
 ## code your solution here. 
+class Cat 
+  
+  def name 
+    name
+  end
+  
+  def name=(name)
+    name=name
+  end
+  
+  def meow
+    puts "meow"
+  end
+end
+  
+  felix = Cat.new 
+  felix.name = "felix"
+  puts felix.name
+  felix.meow
